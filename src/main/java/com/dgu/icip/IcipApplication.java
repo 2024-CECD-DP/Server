486f1af7fa52df0588b97ada62d9f6ca06345052
Limwngur <ovg07047@naver.com>
2024-04-18 10:53:12 +0900

    init : 프로젝트 생성 및 PostgreSQL .yml 설정

---
+++ src/main/java/com/dgu/icip/IcipApplication.java
@@ -0,0 +1,13 @@
+package com.dgu.icip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgu/icip/IcipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgu.icip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
